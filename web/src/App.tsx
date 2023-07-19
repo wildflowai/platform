653359b9cd19286039afbf1c84ee56b557d6521b
Sergei Nozdrenkov <nozdrenkov@gmail.com>
2023-07-19 19:05:19 +0100

    Simple react typescript tailwind app.

---
+++ web/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <h1 className="text-3xl font-bold underline text-red-600">
+      Simple React Typescript Tailwind Sample
+    </h1>
+  );
+}
+
+export default App;
