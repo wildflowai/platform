38be32db127460a21350d95154c15eeaaf2b4a8f
Sergei Nozdrenkov <nozdrenkov@gmail.com>
2024-01-16 22:02:07 +0000

    Support BigQuery execution from GraphQL API.

--- graphql/src/graphql.rs
+++ graphql/src/graphql.rs
@@ -1,4 +1,6 @@
+use anyhow::Result;
 use async_graphql::{Context, EmptyMutation, EmptySubscription, Object, Schema, SimpleObject};
+use gcp_bigquery_client::{model::query_request::QueryRequest, Client};
 
 /// A single column within a database table.
 #[derive(SimpleObject)]
@@ -16,28 +18,39 @@ struct Table {
     columns: Vec<TableColumn>,
 }
 
+/// Executes an arbitrary SQL query on Google BigQuery.
+async fn execute_bigquery_query(query: &str) -> Result<String> {
+    let client = Client::from_service_account_key_file("bigquery-sa.json").await?;
+    client
+        .job()
+        .query("wildflow-pelagic", QueryRequest::new(query.to_string()))
+        .await?;
+
+    Ok("Query executed successfully".to_string())
+}
+
 pub struct QueryRoot;
 
 #[Object]
 impl QueryRoot {
     /// Fetches a list of tables available in the database.
-    ///
-    /// This query will return all tables along with their columns.
     async fn tables(&self, _ctx: &Context<'_>) -> Vec<Table> {
-        vec![
-            Table {
-                name: "Table1".to_string(),
-                columns: vec![
-                    TableColumn {
-                        name: "Column1".to_string(),
-                    },
-                    TableColumn {
-                        name: "Column2".to_string(),
-                    },
-                ],
-            },
-            // Add more tables as needed
-        ]
+        vec![Table {
+            name: "Table1".to_string(),
+            columns: vec![
+                TableColumn {
+                    name: "Column1".to_string(),
+                },
+                TableColumn {
+                    name: "Column2".to_string(),
+                },
+            ],
+        }]
+    }
+
+    /// Executes an arbitrary SQL query on Google BigQuery and returns a simple message.
+    async fn execute_bigquery_query(&self, _ctx: &Context<'_>, query: String) -> Result<String> {
+        execute_bigquery_query(&query).await
     }
 }
 
