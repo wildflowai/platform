1e1c3a4243b25d8bc4d079653288967b8e8babc4
Sergei Nozdrenkov <nozdrenkov@gmail.com>
2024-01-16 22:25:53 +0000

    GraphQL API that works

--- graphql/src/graphql.rs
+++ graphql/src/graphql.rs
@@ -1,6 +1,7 @@
 use anyhow::Result;
 use async_graphql::{Context, EmptyMutation, EmptySubscription, Object, Schema, SimpleObject};
 use gcp_bigquery_client::{model::query_request::QueryRequest, Client};
+use serde_json::Value;
 
 /// A single column within a database table.
 #[derive(SimpleObject)]
@@ -18,15 +19,34 @@ struct Table {
     columns: Vec<TableColumn>,
 }
 
-/// Executes an arbitrary SQL query on Google BigQuery.
-async fn execute_bigquery_query(query: &str) -> Result<String> {
+/// Executes an arbitrary SQL query on Google BigQuery and returns the results as JSON.
+async fn execute_bigquery_query(query: &str) -> Result<Vec<Value>> {
     let client = Client::from_service_account_key_file("bigquery-sa.json").await?;
-    client
+    let mut response = client
         .job()
         .query("wildflow-pelagic", QueryRequest::new(query.to_string()))
         .await?;
 
-    Ok("Query executed successfully".to_string())
+    let results = (0..response.row_count())
+        .filter_map(|_| {
+            response.next_row().then(|| {
+                response
+                    .column_names()
+                    .iter()
+                    .map(|column_name| {
+                        let column_value = response
+                            .get_serde_by_name::<Value>(column_name)
+                            .unwrap_or(None)
+                            .unwrap_or(Value::Null);
+                        (column_name.to_string(), column_value)
+                    })
+                    .collect::<serde_json::Map<_, _>>()
+            })
+        })
+        .map(Value::Object)
+        .collect();
+
+    Ok(results)
 }
 
 pub struct QueryRoot;
@@ -48,8 +68,12 @@ impl QueryRoot {
         }]
     }
 
-    /// Executes an arbitrary SQL query on Google BigQuery and returns a simple message.
-    async fn execute_bigquery_query(&self, _ctx: &Context<'_>, query: String) -> Result<String> {
+    /// Executes an arbitrary SQL query on Google BigQuery and returns the results as JSON.
+    async fn execute_bigquery_query(
+        &self,
+        _ctx: &Context<'_>,
+        query: String,
+    ) -> Result<Vec<Value>> {
         execute_bigquery_query(&query).await
     }
 }
