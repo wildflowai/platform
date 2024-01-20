15aa21da00f3fc991c830b7c2fe13e6d678f2378
Sergei Nozdrenkov <nozdrenkov@gmail.com>
2024-01-20 17:59:46 +0000

    Add GraphiQL API explorer.

--- graphql/src/main.rs
+++ graphql/src/main.rs
@@ -3,8 +3,11 @@ mod graphql;
 mod models;
 
 use crate::graphql::{create_schema, MySchema};
+use async_graphql::http::GraphiQLSource;
 use async_graphql_axum::{GraphQLRequest, GraphQLResponse};
-use axum::{routing::post, Extension, Router};
+use axum::response::{Html, IntoResponse};
+use axum::routing::{get, post};
+use axum::{Extension, Router};
 use dotenv::dotenv;
 use std::net::SocketAddr;
 use std::sync::Arc;
@@ -14,6 +17,7 @@ async fn main() {
     dotenv().ok();
     let schema = Arc::new(create_schema());
     let app = Router::new()
+        .route("/graphql/explore", get(graphiql_handler))
         .route("/graphql", post(graphql_handler))
         .layer(Extension(schema));
 
@@ -31,4 +35,8 @@ async fn main() {
 
 async fn graphql_handler(schema: Extension<Arc<MySchema>>, req: GraphQLRequest) -> GraphQLResponse {
     schema.execute(req.into_inner()).await.into()
+}
+
+pub async fn graphiql_handler() -> impl IntoResponse {
+    Html(GraphiQLSource::build().endpoint("/graphql").finish())
 }
