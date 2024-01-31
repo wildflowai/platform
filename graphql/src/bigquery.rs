9bce5d58a9d73aa03c9b6fe38a2d1f13db5b31c2
Sergei Nozdrenkov <nozdrenkov@gmail.com>
2024-01-31 17:13:25 +0000

    Func to request BQ table metadata.

--- graphql/src/bigquery.rs
+++ graphql/src/bigquery.rs
@@ -1,4 +1,5 @@
-use anyhow::{Error, Result};
+use anyhow::{anyhow, Error, Result};
+use async_graphql::SimpleObject;
 use futures::stream::{self, StreamExt};
 use gcp_bigquery_client::{
     dataset::ListOptions as DatasetListOptions, model::query_request::QueryRequest,
@@ -115,6 +116,71 @@ pub async fn execute_bigquery_query(query: &str) -> Result<Vec<Value>> {
     Ok(results)
 }
 
+#[derive(SimpleObject, PartialEq, Debug)]
+pub struct TableInfo {
+    pub table_name: String,
+    pub dataset_name: String,
+    pub created_millis: f64,
+    pub total_rows: u64,
+    pub total_logical_bytes: i64,
+    pub total_physical_bytes: i64,
+    pub last_modified_millis: f64,
+}
+
+// A macro for parsing fields
+macro_rules! parse_field {
+    ($row:expr, $field_name:expr, $type:ty) => {
+        $row[$field_name]
+            .as_str()
+            .ok_or_else(|| anyhow!("Missing '{}'", $field_name))?
+            .parse::<$type>()
+            .map_err(|_| anyhow!("Invalid format for '{}'", $field_name))?
+    };
+}
+
+pub async fn tables_info(
+    token: &str,
+    project: &str,
+    query_filter: Option<&str>,
+) -> Result<Vec<TableInfo>, Error> {
+    let default_region = "region-us";
+    let query = format!(
+        r#"
+        SELECT
+            t.table_name,
+            t.table_schema AS dataset_name,
+            t.creation_time AS created_millis,
+            t.total_rows,
+            t.total_logical_bytes,
+            t.total_physical_bytes,
+            t.storage_last_modified_time AS last_modified_millis
+        FROM `{}.{}.INFORMATION_SCHEMA.TABLE_STORAGE` t
+        WHERE t.project_id = '{}' {}
+        "#,
+        project,
+        default_region,
+        project,
+        query_filter.unwrap_or_default()
+    );
+
+    execute_bigquery_query_bearer(token, project, &query)
+        .await?
+        .into_iter()
+        .map(|row| {
+            let table_info = TableInfo {
+                table_name: parse_field!(row, "table_name", String),
+                dataset_name: parse_field!(row, "dataset_name", String),
+                created_millis: parse_field!(row, "created_millis", f64),
+                total_rows: parse_field!(row, "total_rows", u64),
+                total_logical_bytes: parse_field!(row, "total_logical_bytes", i64),
+                total_physical_bytes: parse_field!(row, "total_physical_bytes", i64),
+                last_modified_millis: parse_field!(row, "last_modified_millis", f64),
+            };
+            Ok(table_info)
+        })
+        .collect()
+}
+
 /// Executes an arbitrary SQL query on Google BigQuery using Bearer token authentication and returns
 /// the results as JSON.
 pub async fn execute_bigquery_query_bearer(
@@ -201,9 +267,13 @@ mod tests {
     async fn test_run_query_auth_basic_select_ok() {
         let expected = vec![json!({"numbers": "123", "letters": "abc"})];
 
-        let tested = run_query("select 123 as numbers, 'abc' as letters")
-            .await
-            .expect("Query failed");
+        let tested = execute_bigquery_query_bearer(
+            &token(),
+            &project(),
+            "select 123 as numbers, 'abc' as letters",
+        )
+        .await
+        .expect("Query failed");
 
         assert_eq!(tested, expected);
     }
@@ -217,7 +287,9 @@ mod tests {
             ";
         let expected: Vec<Value> = vec![];
 
-        let tested = run_query(query).await.expect("Query failed");
+        let tested = execute_bigquery_query_bearer(&token(), &project(), query)
+            .await
+            .expect("Query failed");
 
         assert_eq!(tested, expected);
     }
@@ -226,7 +298,7 @@ mod tests {
     async fn test_run_query_auth_incorrect_query_error() {
         let query = "some incorrect query";
 
-        let result = run_query(query).await;
+        let result = execute_bigquery_query_bearer(&token(), &project(), query).await;
 
         match result {
             Ok(_) => panic!("Expected an error but query succeeded"),
@@ -238,9 +310,43 @@ mod tests {
         }
     }
 
-    async fn run_query(query: &str) -> Result<Vec<Value>> {
-        let token = std::env::var("GCP_ACCESS_TOKEN").expect("GCP_ACCESS_TOKEN not set");
-        let project = std::env::var("WILDFLOW_BIGQUERY_TEST_PROJECT").unwrap();
-        execute_bigquery_query_bearer(&token, &project, query).await
+    #[tokio::test]
+    async fn test_tables_info_returned() {
+        let create_table_query = "
+            create or replace table raw.test_table_info_table as (
+                select 123 as numbers, 'abc' as letters
+            );
+            ";
+        execute_bigquery_query_bearer(&token(), &project(), create_table_query)
+            .await
+            .expect("Query failed");
+
+        let tested = tables_info(
+            &token(),
+            &project(),
+            Some("and t.table_name = 'test_table_info_table'"),
+        )
+        .await
+        .expect("Query failed");
+
+        print!("{:?}", tested);
+
+        assert_eq!(tested.len(), 1);
+        let result = &tested[0];
+        assert_eq!(result.table_name, "test_table_info_table");
+        assert_eq!(result.dataset_name, "raw");
+        assert_eq!(result.total_rows, 1);
+        assert!(result.total_logical_bytes > 10);
+        assert!(result.total_physical_bytes > 9000);
+        assert!(result.created_millis.is_sign_positive());
+        assert!(result.last_modified_millis.is_sign_positive());
+    }
+
+    fn project() -> String {
+        std::env::var("WILDFLOW_BIGQUERY_TEST_PROJECT").unwrap()
+    }
+
+    fn token() -> String {
+        std::env::var("GCP_ACCESS_TOKEN").expect("GCP_ACCESS_TOKEN not set")
     }
 }
