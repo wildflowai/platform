e60951feca2a00d8637ce6fc1fa3e6c46605d844
Sergei Nozdrenkov <nozdrenkov@gmail.com>
2024-09-21 22:14:56 +0100

    Add progressive loading

--- 3d/main.js
+++ 3d/main.js
@@ -1,12 +1,12 @@
 // main.js
 import * as GaussianSplats3D from "@mkkellogg/gaussian-splats-3d";
 
-// Update the camera parameters with your desired values
 const viewer = new GaussianSplats3D.Viewer({
   cameraUp: [0.24929, -0.2672, -0.93084],
   initialCameraPosition: [-3.93951, 0.24631, -3.29199],
   initialCameraLookAt: [-1.01181, 0.18365, 4.45069],
   rootElement: document.getElementById("viewer"),
+  sceneRevealMode: GaussianSplats3D.SceneRevealMode.Gradual,
 });
 
 viewer
@@ -16,6 +16,7 @@ viewer
     position: [0, 1, 0],
     rotation: [0, 0, 0, 1],
     scale: [1.5, 1.5, 1.5],
+    progressiveLoad: true,
   })
   .then(() => {
     viewer.start();
