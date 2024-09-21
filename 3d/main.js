70a3eab95704368b96935348480c346c4d194ec9
Sergei Nozdrenkov <nozdrenkov@gmail.com>
2024-09-21 22:56:29 +0100

    ModelId from URL

--- 3d/main.js
+++ 3d/main.js
@@ -1,6 +1,15 @@
 // main.js
 import * as GaussianSplats3D from "@mkkellogg/gaussian-splats-3d";
 
+const pathSegments = window.location.pathname.split("/").filter(Boolean);
+const modelId = pathSegments[0];
+
+if (!modelId) {
+  window.location.href = "/W32Em7";
+}
+
+const modelUrl = `https://storage.googleapis.com/wildflow/${modelId}/splats.ksplat`;
+
 const viewer = new GaussianSplats3D.Viewer({
   cameraUp: [0.24929, -0.2672, -0.93084],
   initialCameraPosition: [-3.93951, 0.24631, -3.29199],
@@ -11,17 +20,14 @@ const viewer = new GaussianSplats3D.Viewer({
 });
 
 viewer
-  .addSplatScene(
-    "https://storage.googleapis.com/wildflow/W32Em7/splats.ksplat",
-    {
-      splatAlphaRemovalThreshold: 5,
-      showLoadingUI: true,
-      position: [0, 1, 0],
-      rotation: [0, 0, 0, 1],
-      scale: [1.5, 1.5, 1.5],
-      progressiveLoad: true,
-    }
-  )
+  .addSplatScene(modelUrl, {
+    splatAlphaRemovalThreshold: 5,
+    showLoadingUI: true,
+    position: [0, 1, 0],
+    rotation: [0, 0, 0, 1],
+    scale: [1.5, 1.5, 1.5],
+    progressiveLoad: true,
+  })
   .then(() => {
     viewer.start();
   });
